482023bad290dc97dbdd91326eea40fce46fb3a4
shanmughamg <shanmughaharishg@gmail.com>
2022-04-13 01:33:07 +0530

    main

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const ExpenseDate = (props) => {
-  const month ="march"; //props.date;
-  const day = 10//props.date;
-  const year = 2022//props.date;
+    const dates=new Date(props.date);
+  //const month =dates.getMonth();
+  const day = dates.getDay();//props.date;
+  const year = dates.getFullYear();//props.date;
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
+  //const d = new Date();
+  let months = month[dates.getMonth()];
+  
   return (
-    <div class="d-block">
-      <div class="d-block">{month}</div>
-      <div class="d-block">{year}</div>
-      <div class="d-block">{day}</div>
+    <div class="btn-dark col-2 rounded">
+      <div class="flex-grow-1">{months}</div>
+      <div class="flex-grow-1">{year}</div>
+      <div class="flex-grow-1">{day}</div>
     </div>
   );
 };
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,29 +6,32 @@ import ExpensesFilter from './ExpensesFilter';
 
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        };
 
-  return (
-    <div>
-      <Card class="card">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
-    </div>
-  );
+        const filteteredex = props.items.filter(expense => { return expense.date.getFullYear().toString() === filteredYear; })
+    
+     return (
+        <div>
+            <Card class="card">
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                {filteteredex.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
+            </Card>
+        </div>
+    );
 };
 
 export default Expenses;
